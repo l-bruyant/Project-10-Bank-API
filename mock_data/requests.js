2539df15067e82607a89a3825093e73bc42653f6
Lbrt64 <lbruyant@outlook.fr>
2022-12-21 10:06:51 +0100

    Improved html / css designs

---
+++ mock_data/requests.js
@@ -0,0 +1,20 @@
+// LOG A USER POST
+// address : http://localhost:3001/api/v1/user/login
+// body : 
+// {
+//     "email": "tony@stark.com",
+//     "password": "password123"
+//   }
+
+// GET A USER PROFILE POST 
+// address : http://localhost:3001/api/v1/user/profile
+// headers => Authorization Bearer Token
+
+// MODIFIY A USER DATA PUT
+// address : http://localhost:3001/api/v1/user/profile
+// headers => Authorization Bearer Token
+// body : 
+// {
+//     "firstName": "string",
+//     "lastName": "string"
+//   }
